fd36b85bef8c35ac2463ca3d2490db7c3d171bab
aduban <aduban15@gmail.com>
2016-03-26 15:57:09 -0500

    inicio repositorio

---
+++ code.js
@@ -0,0 +1,12 @@
+console.log("corriendo..");
+alert("corriendo..");
+l = prompt("cual es tu numero");
+document.write("<h1>tabla de multiplicar del " + l + "<br>" + "</h1>");
+for (i=0; i<=10; i++){
+  function multiplicar(x){
+    return x * i;
+  }
+  
+  document.write(l +" por "+ i + " es "+ multiplicar(l)+"<br>");
+  }
+
